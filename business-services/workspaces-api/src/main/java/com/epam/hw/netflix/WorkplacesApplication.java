f816549a44fd0e86fea88ddb43a63eb222e263c7
Aleksandr <chamtru@gmail.com>
2020-06-15 10:07:51 +0400

    homework complite

--- business-services/workspaces-api/src/main/java/com/epam/hw/netflix/WorkplacesApplication.java
+++ business-services/workspaces-api/src/main/java/com/epam/hw/netflix/WorkplacesApplication.java
@@ -2,8 +2,9 @@ package com.epam.hw.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class WorkplacesApplication {
 	public static void main(String[] args) {
